f28c33623c278e6b673c2409c59f9197572e0206
Kell22-mkt <kellyckaroline@gmail.com>
2022-06-29 15:01:45 -0300

    primeirodesafio

---
+++ script.js
@@ -0,0 +1,15 @@
+//crie uma variavel que receba um nome
+var name ="kelly"
+//crie uma variavel que receba um numero
+var number = 8
+//creie uma variavel com o ano atual e subtraia pelo um numero que voce guardou
+var year = 2022
+var sub = number-year;{
+console.log(number-year)
+}
+
+//crie uma variavel que se chama quartaFeira e atribula no valor boleano e mostre a saida no console
+var amor = "quartaFeira";
+if('true'){
+console.log("quartaFeira")
+}
